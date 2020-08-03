62488929ba55727e5fede3e93d6a98bd31266113
Ryan Steed <rsteed11@gmail.com>
2020-08-03 17:56:34 -0400

    :arrow_upper_right: Package, script for continuous image interpretation

---
+++ scripts/interpret_images.py
@@ -0,0 +1,23 @@
+from regression.api import interpret
+
+# Random Face Validation
+interpret(
+	'Trustworthy', 
+	['data/maxdistinct_aligned'], 
+	'data/random_aligned', 
+	file='png',
+	ground_truth=False,
+	save=True,
+	num_samples=5000
+)
+
+# Politician Bias
+interpret(
+    'Competent', 
+    ['data/maxdistinct_aligned'], 
+    '../data/politicians-database_aligned',
+    file='png',
+    ground_truth=False,
+    save=True,
+    num_samples=5000
+)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='regression',
+    version='0.1',
+    packages=['regression',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
