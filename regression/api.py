cf891abf48d4a7ced720d94da313c2035cb6d499
Ryan Steed <rsteed11@gmail.com>
2020-08-03 16:42:38 -0400

    :sparkles: Interpretation API endpoint

--- regression/api.py
+++ regression/api.py
@@ -1,5 +1,6 @@
 from regression.feature_extraction import FeatureExtractor, LabelLoader
 from regression.model import Regressor
+from regression.interpretation import Interpreter
 import os
 import pickle
 import pandas as pd
@@ -61,6 +62,38 @@ def regress_single(label, image_dirs, test_dir=None, cross_validate=False):
 
     return
 
+def interpret(label, training_dirs, interpret_dir, file='jpg', n=None, ground_truth=True, save=False, **kwargs):
+    reg = get_regressor(label, training_dirs)
+    X = reg.X
+    y = reg.y
+    interpreter = Interpreter()
+    interpreter.fit(X, y)
+
+    features_interpret = FeatureExtractor(interpret_dir).get_features()
+    if ground_truth:
+        # fix this later - something wrong with loading if _aligned not used
+        labels = LabelLoader(interpret_dir).get_labels(normalization=True)
+        features_interpret = merge_x_y(features_interpret, labels).dropna(subset=[label])
+
+    samples = features_interpret if n is None else features_interpret.sample(n)
+    for i, sample in samples.iterrows():
+        print(*sample[['Face name', 'Source']])
+        sample_img = interpreter.get_img(
+            *sample[['Face name', 'Source']],
+            subd=os.path.basename(interpret_dir) if sample['Source'] != os.path.basename(interpret_dir) else None,
+            d=os.path.dirname(interpret_dir),
+            file=file
+        )
+        print(sample['Face name'])
+        interpreter.explain_img(
+            sample_img, 
+            label=label,
+            name=sample['Face name'],
+            ground_truth = sample[label] if ground_truth else None,
+            **kwargs
+        )
+
+
 
 def get_regressor(label, image_dirs):
     print("Generating {} regressor".format(label))
@@ -89,7 +122,7 @@ def get_regressor(label, image_dirs):
 
         pickle.dump(reg, open(filename, 'wb'))
 
-        return reg
+        return
 
 
 def merge_x_y(X, y):
